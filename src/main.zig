0c7a5b275d6785cc903c6b4cf12b015f77bf4c3a
Habib Herdiansyah <expduta@gmail.com>
2026-01-26 10:37:01 +0700

    on progress

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,8 @@
-const std = @import("std");
-
+const command = @import("help.zig");
 
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    try command.init();
 }
 
 
